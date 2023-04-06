bc68cd51b0c404e5a62f114eaf587318f788ffa7
Wander Hungerbuhler <whfdev@gmail.com>
2023-04-06 14:42:32 +0100

    Rifa do Wander

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -28,7 +29,11 @@ export default class MyDocument extends Document {
     }
   }
 
+
+
   render() {
+    const GTM_ID = "G-JH7BQH929Q";
+
     return (
       <Html lang="en">
         <Head>
@@ -38,6 +43,23 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet"
           />
           <link rel="shortcut icon" href="/shorticon.png" />
+
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2906000400105513"
+            crossOrigin="anonymous"
+          />
+
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `}
+          </Script>
+
         </Head>
         <body>
           <Main />
